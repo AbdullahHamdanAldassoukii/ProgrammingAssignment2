a8e9a1b329b61746886d47398247157f59b2de72
Abdullah Hamdan Aldassoukii <abdullah.hamdan.aldassouki@gmail.com>
2016-09-22 03:30:36 +0300

    My project

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## initiat the inv vairable
+        inv<-NULL
+        
+        ## set the spicial matrix..
+        set<-function(y){ x<<-y }
+        
+        ## get the spicial matrix..
+        get<-function(){ x }
+        
+        ## assigne the inverse of te spicial matrix to "inv"
+        set_invers<-function(inverse){ inv<<-inverse }
+        
+        ## get the value of "inv"
+        get_invers<-function(){inv }
+        
+        list(set = set, get = get,
+             set_invers=set_invers
+             ,get_invers=get_invers)
 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+        
+        inv <- x$get_invers()
+        
+        if(!is.null(inv)){
+                message("getting the invers")
+                return(inv)
+                
+        }## end if
+        
+        data<-x$get()
+        inv<-solve(data,...)
+        x$set_invers(inv)
+        inv
 }
